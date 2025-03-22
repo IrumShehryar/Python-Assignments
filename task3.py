483856b15bcac51537e7d4458faa5b7e02aa52d7
IrumShehryar <irum.shehryar@gmail.com>
2025-03-23 01:07:03 +0200

    Delete task3.py

--- task3.py
+++
@@ -1,6 +0,0 @@
-length=float(input("Enter the length of the rectangle: "))
-width=float(input("Enter the width of the rectangle: "))
-area=length*width
-perimeter=2*(length+width)
-print(f'The Area of a rectangle is: {area:.2f}')
-print(f'The perimeter of a rectangle is: {perimeter:.2f}')
